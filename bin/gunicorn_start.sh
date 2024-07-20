a7d87b56737d464364c95920cfc33000f198b9ca
Rishav Choudhary <156159382+Rishavenigma@users.noreply.github.com>
2024-07-20 13:07:41 +0530

    Update gunicorn_start.sh

--- bin/gunicorn_start.sh
+++ bin/gunicorn_start.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
-NAME="project_settings"                                  # Name of the application
-DJANGODIR=/app             # Django project directory
-SOCKFILE=/app/run/gunicorn.sock  # we will communicte using this unix socket
-NUM_WORKERS=3                                     # how many worker processes should Gunicorn spawn
-DJANGO_SETTINGS_MODULE=project_settings.settings             # which settings file should Django use
-DJANGO_WSGI_MODULE=project_settings.wsgi                     # WSGI module name
+NAME="project_settings"                                  
+DJANGODIR=/app             
+SOCKFILE=/app/run/gunicorn.sock  
+NUM_WORKERS=3                                     
+DJANGO_SETTINGS_MODULE=project_settings.settings             
+DJANGO_WSGI_MODULE=project_settings.wsgi                     
 
 echo "Starting $NAME as `whoami`"
 
-# Create the run directory if it doesn't exist
 RUNDIR=$(dirname $SOCKFILE)
 test -d $RUNDIR || mkdir -p $RUNDIR
 
-# Start your Django Gunicorn
  gunicorn project_settings.wsgi:application --bind=unix:$SOCKFILE --workers $NUM_WORKERS --timeout 600
